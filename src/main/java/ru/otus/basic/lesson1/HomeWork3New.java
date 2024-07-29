d94068a935e0fdc2094c88de977b1d561ebc0f07
yutta13 <dodik13@gmail.com>
2024-07-29 15:39:20 +0300

    otus-Homework_3_full_fix_errors

--- src/main/java/ru/otus/basic/lesson1/HomeWork3New.java
+++ src/main/java/ru/otus/basic/lesson1/HomeWork3New.java
@@ -1,6 +1,5 @@
 package ru.otus.basic.lesson1;
 
-import java.util.Arrays;
 
 public class HomeWork3New {
     public static void main(String[] args) {
@@ -8,9 +7,10 @@ public class HomeWork3New {
         sumOfPositiveElements(new int[][]{{1, -2, -3}, {4, 0, 6}});
         squareArrayInt(5);
         nullDiagonalElements(new int[][]{{1, 2, 3, 5, 6}, {1, 2, 3, 5, 6}, {1, 2, 3, 5, 6}, {1, 2, 3, 5, 6}, {1, 2, 3, 5, 6}});
-        findMax(new int[][] {{1, 2, 3, 5, 66}, {118, 12,13, 14, 16}, {21, 22, 72, 25, 26}, {31, 32, 33, 35, 36}, {41, 42, 84, 45, 46}});
-        sumSecondLineElements(new int[][]{{1, 2, 3, 5, 66}, {31, 32, 33, 35, 36}, {41, 42, 84, 45, 46}});
-        sumSecondLineElements(new int[][]{{1, 2, 3, 5, 66},{31, 32, 33, 35, 36}});
+        findMax(new int[][]{{-11, -2, -3, -5, -66}, {-118, -12, -13, -14, -16}, {-21, -22, -72, -25, -26}, {-31, -32, -33, -35, -36}, {-41, -42, -84, -45, -46}});
+        findMax(new int[][]{{-11, -2, -3, -5, -66}, {-118, -12, -13, -14, 516}, {-21, -22, -72, 25, -26}, {-31, -32, 33, 35, 36}, {-41, 42, 84, 45, 46}});
+        sumSecondLineElements(new int[][]{{1, 2, 3, 5, 66}, {31, 32, 33, 35, 36}, {1, 2, 1, 2, 1}});
+        sumSecondLineElements(new int[][]{{1, 2, 3, 5, 66}, {31, 32, 33, 35, 36}});
     }
 
     public static int sumOfPositiveElements(int[][] arr) {
@@ -29,12 +29,10 @@ public class HomeWork3New {
     }
 
     public static void squareArrayInt(int n) {
-        char[][] arr = new char[n][n];
         System.out.println("exercise 2");
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
-                arr[i][j] = '*';
-                System.out.print(arr[i][j] + " ");
+                System.out.print(" * ");
             }
             System.out.println();
         }
@@ -46,7 +44,7 @@ public class HomeWork3New {
 
         for (int i = 0; i < arr.length; ++i) {
             for (int j = 0; j < arr[i].length; ++j) {
-                if (i == arr.length || j == i) {
+                if (j == i) {
                     arr[i][j] = 0;
                 }
                 System.out.print(arr[i][j] + " ");
@@ -58,13 +56,12 @@ public class HomeWork3New {
 
     public static int findMax(int[][] array) {
         System.out.println("exercise 4");
-        int maxElement = 0;
+        int maxElement = Integer.MIN_VALUE;
         for (int i = 0; i < array.length; ++i) {
             for (int j = 0; j < array.length; ++j) {
                 if (array[i][j] > maxElement) {
                     maxElement = array[i][j];
                 }
-                ;
             }
         }
         System.out.println(maxElement);
@@ -73,13 +70,19 @@ public class HomeWork3New {
 
     public static int sumSecondLineElements(int[][] array) {
         System.out.println("exercise 5");
-        int sum = -1;
+        int sum = 0;
+        if (array.length <= 2) {
+            sum = -1;
+            System.out.println(sum);
+            return sum;
+        } else
 
-        if (array.length > 2){
-            for (int j = 0; j < array.length; ++j) {
-            sum += array[2][j];}
-        }
-        System.out.println(sum);
+            for (int i = 2; i < array.length; ++i) {
+                for (int j = 0; j < array[i].length; ++j) {
+                    sum += array[i][j];
+                }
+                System.out.println(sum);
+            }
         return sum;
     }
 }
