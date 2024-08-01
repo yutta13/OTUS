91aadfe7ee6047854bb9e7bea42d3268bbbb47d7
yutta13 <dodik13@gmail.com>
2024-08-01 12:53:01 +0300

    otus-modul2 started

--- src/main/java/ru/otus/basic/modul2/Mainapp.java
+++ src/main/java/ru/otus/basic/modul2/Mainapp.java
@@ -5,20 +5,41 @@ import java.util.Arrays;
 public class Mainapp {
     public static void main(String[] args) {
 
-        User[] user1 =new User[]{"Ivan", "Mikovich", "Kashtanov", "mail@mail.ru", 39};
+        User[][] userArray = new User[][]{
+                {new User("Ivan", "Mikovich", "Kashtanov", "mail@mail.ru", 39)},
+                {new User("Nik2", "Mikovich", "Kashtanov", "mail@mail.ru", 20)},
+                {new User("Nik3", "Mikovich", "Kashtanov", "mail@mail.ru", 17)},
+                {new User("Nik4", "Mikovich", "Kashtanov", "mail@mail.ru", 56)},
+                {new User("Nik5", "Mikovich", "Kashtanov", "mail@mail.ru", 12)},
+                {new User("Nik6", "Mikovich", "Kashtanov", "mail@mail.ru", 1)},
+                {new User("Nik7", "Mikovich", "Kashtanov", "mail@mail.ru", 99)},
+                {new User("Nik8", "Mikovich", "Kashtanov", "mail@mail.ru", 105)},
+                {new User("Nik9", "Mikovich", "Kashtanov", "mail@mail.ru", 00)},
+                {new User("Nik10", "Mikovich", "Kashtanov", "mail@mail.ru", 40)}
+        };
 
 
+   }
 
 
-  //      User[] user2 =new User[]{"22", "Mikovich", "Kashtanov", "mail@mail.ru", String.valueOf(39)};
-  //      String[][] usersArray = new String[][]{{user1}, {user2}};
- // System.out.println(Arrays.deepToString(user1));
+        public static void usersAge (User[][] userArray){
+            for (int i = 0; i < userArray.length ; i++) {
+                for (int j = 4; j < userArray[i].length; j++) {
+                    if (userArray[i][j] == 40 ){
+                        System.out.println("ok");
+                    }
+                }
 
+            }
+//            for (int i = 0; i < userArray.length; i++) {
+//                System.out.println(Arrays.deepToString(userArray));
+//            }
+
+        }
+        ;
     }
-}
 
 //      User[] user2 = new User{"Nik2", "Mikovich", "Kashtanov", "mail@mail.ru",20};
-
 //        User[] user3 = new User("Nik3", "Mikovich", "Kashtanov", "mail@mail.ru",17  );
 //        User[] user4 = new User("Nik4", "Mikovich", "Kashtanov", "mail@mail.ru",56  );
 //        User[] user5 = new User("Nik5", "Mikovich", "Kashtanov", "mail@mail.ru",12  );
@@ -28,6 +49,18 @@ public class Mainapp {
 //        User[] user9 = new User("Nik9", "Mikovich", "Kashtanov", "mail@mail.ru",00  );
 //        User[] user10 = new User("Nik10", "Mikovich", "Kashtanov", "mail@mail.ru",40  );
 
+//    User[] userArray = new User[]{
+//            new User("Ivan", "Mikovich", "Kashtanov", "mail@mail.ru", 39),
+//            new User("Nik2", "Mikovich", "Kashtanov", "mail@mail.ru", 20),
+//            new User("Nik3", "Mikovich", "Kashtanov", "mail@mail.ru", 17),
+//            new User("Nik4", "Mikovich", "Kashtanov", "mail@mail.ru", 56),
+//            new User("Nik5", "Mikovich", "Kashtanov", "mail@mail.ru", 12),
+//            new User("Nik6", "Mikovich", "Kashtanov", "mail@mail.ru", 1),
+//            new User("Nik7", "Mikovich", "Kashtanov", "mail@mail.ru", 99),
+//            new User("Nik8", "Mikovich", "Kashtanov", "mail@mail.ru", 105),
+//            new User("Nik9", "Mikovich", "Kashtanov", "mail@mail.ru", 00),
+//            new User("Nik10", "Mikovich", "Kashtanov", "mail@mail.ru", 40)};
+//}
 
 //    sumOfPositiveElements(new int[][]{{1, -2, -3}, {4, 0, 6}});
 //        squareArrayInt(5);
