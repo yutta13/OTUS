a7522c3493a71480d47024a6c0b2dfc1992bb83d
yutta13 <dodik13@gmail.com>
2024-08-01 12:20:41 +0300

    otus-modul2 started

--- src/main/java/ru/otus/basic/modul2/Mainapp.java
+++ src/main/java/ru/otus/basic/modul2/Mainapp.java
@@ -5,8 +5,19 @@ import java.util.Arrays;
 public class Mainapp {
     public static void main(String[] args) {
 
-        User[] user1 = new User{"22", "Mikovich", "Kashtanov", "mail@mail.ru",39};
-        User[] user2 = new User{"Nik2", "Mikovich", "Kashtanov", "mail@mail.ru",20};
+        User[] user1 =new User[]{"Ivan", "Mikovich", "Kashtanov", "mail@mail.ru", 39};
+
+
+
+
+  //      User[] user2 =new User[]{"22", "Mikovich", "Kashtanov", "mail@mail.ru", String.valueOf(39)};
+  //      String[][] usersArray = new String[][]{{user1}, {user2}};
+ // System.out.println(Arrays.deepToString(user1));
+
+    }
+}
+
+//      User[] user2 = new User{"Nik2", "Mikovich", "Kashtanov", "mail@mail.ru",20};
 
 //        User[] user3 = new User("Nik3", "Mikovich", "Kashtanov", "mail@mail.ru",17  );
 //        User[] user4 = new User("Nik4", "Mikovich", "Kashtanov", "mail@mail.ru",56  );
@@ -16,9 +27,13 @@ public class Mainapp {
 //        User[] user8 = new User("Nik8", "Mikovich", "Kashtanov", "mail@mail.ru",105  );
 //        User[] user9 = new User("Nik9", "Mikovich", "Kashtanov", "mail@mail.ru",00  );
 //        User[] user10 = new User("Nik10", "Mikovich", "Kashtanov", "mail@mail.ru",40  );
-        User[][] usersArray = {{user1[user1.length]},{user2[user2.length]}};
 
-        System.out.println(Arrays.deepToString(user1));
 
-    }
-}
+//    sumOfPositiveElements(new int[][]{{1, -2, -3}, {4, 0, 6}});
+//        squareArrayInt(5);
+//        nullDiagonalElements(new int[][]{{1, 2, 3, 5, 6}, {1, 2, 3, 5, 6}, {1, 2, 3, 5, 6}, {1, 2, 3, 5, 6}, {1, 2, 3, 5, 6}});
+//        findMax(new int[][]{{-11, -2, -3, -5, -66}, {-118, -12, -13, -14, -16}, {-21, -22, -72, -25, -26}, {-31, -32, -33, -35, -36}, {-41, -42, -84, -45, -46}});
+//        findMax(new int[][]{{-11, -2, -3, -5, -66}, {-118, -12, -13, -14, 516}, {-21, -22, -72, 25, -26}, {-31, -32, 33, 35, 36}, {-41, 42, 84, 45, 46}});
+//        System.out.println(sumSecondLineElements(new int[][]{{1, 2, 3, 5, 66}, {31, 32, 33, 35, 36}, {1, 2, 1, 2, 1}}));
+//        System.out.println(sumSecondLineElements(new int[][]{{1, 2, 3, 5, 66}, {31, 32, 33, 35, 36}}));
+//        System.out.println(sumSecondLineElements(new int[][]{{1, 2, 3, 5, 66}}));
