5362f215b6362a0cb07b06296a6b6385e0a2c4ef
yutta13 <dodik13@gmail.com>
2024-08-28 14:31:52 +0300

    homework_7_ arraylist2 full

--- src/main/java/ru/otus/m2/HW_7_Array_list2/PhoneBook.java
+++ src/main/java/ru/otus/m2/HW_7_Array_list2/PhoneBook.java
@@ -1,69 +1,65 @@
 package ru.otus.m2.HW_7_Array_list2;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class PhoneBook {
-    private Map<String, Integer> personContact;
+    private Map<String, List<Integer>> personContactMap;
+
     public PhoneBook() {
-        personContact = new HashMap<>();
-    }
-   public void add(String fio, int number) {
-        personContact.put(fio, number);
+        personContactMap = new HashMap<>();
     }
 
+    /**
+     * Метод add() должен позволять добавлять запись имя-телефон;
+     * Под одним именем может быть несколько телефонов (в случае однофамильцев, или наличии
+     * у одного человека нескольких номеров)
+     * @param fio ФИО
+     * @param phoneNumber Номер телефона
+     */
+    public void add(String fio, int phoneNumber) {
+        List<Integer> phoneNumbers = personContactMap.getOrDefault(fio, new ArrayList<>());
+        phoneNumbers.add(phoneNumber);
+        personContactMap.put(fio, phoneNumbers);
+    }
 
+    /**
+     * Метод find() выводит спривочник телефонов
+     */
+    public void info() {
+        System.out.println(personContactMap);
+    }
 
+    /**
+     * Метод find() выполнять поиск номер(-а, -ов) телефона по имени;
+     * Под одним именем может быть несколько телефонов (в случае однофамильцев, или наличии
+     * у одного человека нескольких номеров), тогда при запросе такой фамилии должны выводиться все телефоны;
+     * @param fio ФИО
+     */
+    public void find(String fio) {
+        if (personContactMap.containsKey(fio)) {
+            List<Integer> number = new ArrayList<>(personContactMap.get(fio));
+            System.out.println(Arrays.asList(number));
+        } else {
+            System.out.println("Not founded");
+        }
+    }
 
+    /**
+     * Метод containsPhoneNumber должен проверять наличие телефона в справочнике.
+     * @param phoneNumber номер телефона
+     * @return да/нет наличие телефона в справочнике
+     */
+    public boolean containsPhoneNumber(int phoneNumber) {
+        for (List<Integer> numbers : personContactMap.values()) {
+            if (numbers.contains(phoneNumber)) {
+                System.out.println("Найден телефон " + phoneNumber );
+                return true;
+            }
+        }
+        System.out.println("Не найден телефон ");
+        return false;
+    }
 }
 
-//    public String getFio() {
-//        return fio;
-//    }
-//
-//    public void setFio(String fio) {
-//        this.fio = fio;
-//    }
-//
-//    public int getNumber() {
-//        return number;
-//    }
-//
-//    public void setNumber(int number) {
-//        this.number = number;
-//    }
 
 
-//    List<PhoneBook> PhoneBook = new ArrayList<>(Arrays.asList(
-//            new PhoneBook("Ivan", 30),
-//            new PhoneBook("Nik", 70),
-//            new PhoneBook("Sasha", 20),
-//            new PhoneBook("Borya", 34),
-//            new PhoneBook("Ivan Ivanov", 89),
-//            new PhoneBook("Ivan Ivanov", 89),
-//            new PhoneBook("Nik Mamontov1", 22),
-//            new PhoneBook("Nik Mamontov3", 47)
-//    ));
-
-
-//        Map<String, Integer> phoneBook = new HashMap<>(
-//        new PhoneBook("Ivan Ivanov", 89);
-//        new PhoneBook("Ivan Ivanov", 89);
-//        new PhoneBook("Nik Mamontov", 22);
-//        new PhoneBook("Nik Mamontov", 47);
-//
-//
-
-
-
-
-
-/*
-Реализуйте класс PhoneBook, который хранит в себе список имен (фио) и телефонных номеров;
-
-Метод add() должен позволять добавлять запись имя-телефон;
-Метод find() выполнять поиск номер(-а, -ов) телефона по имени;
-
-Под одним именем может быть несколько телефонов (в случае однофамильцев, или наличии у одного человека нескольких номеров), тогда при запросе такой фамилии должны выводиться все телефоны;
-Метод containsPhoneNumber должен проверять наличие телефона в справочнике.
-*/
