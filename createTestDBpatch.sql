2afd8b5d320f0c28243eb397b584c79605f65570
yutta13 <dodik13@gmail.com>
2024-09-26 17:33:10 +0300

    Update createTestDBpatch.sql

--- createTestDBpatch.sql
+++ createTestDBpatch.sql
@@ -1,3 +1,45 @@
+-- Table: public.test
+
+-- DROP TABLE IF EXISTS public.test;
+
+CREATE TABLE IF NOT EXISTS public.test
+(
+    test_id numeric(999,0) NOT NULL,
+    test_name character varying(256) COLLATE pg_catalog."default" NOT NULL,
+    descx character varying(999) COLLATE pg_catalog."default",
+    CONSTRAINT test_pkey PRIMARY KEY (test_id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.test
+    OWNER to postgres;	
+
+##
+-- Table: public.questions
+
+-- DROP TABLE IF EXISTS public.questions;
+
+CREATE TABLE IF NOT EXISTS public.questions
+(
+    question_id numeric(999,0) NOT NULL,
+    test_id numeric(999,0) NOT NULL,
+    question character varying(40) COLLATE pg_catalog."default",
+    CONSTRAINT questions_pkey PRIMARY KEY (question_id),
+    CONSTRAINT questions_test_id_fkey FOREIGN KEY (test_id)
+        REFERENCES public.test (test_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.questions
+    OWNER to postgres;
+
+
+
 -- Table: public.answers
 
 -- DROP TABLE IF EXISTS public.answers;
@@ -21,41 +63,4 @@ TABLESPACE pg_default;
 ALTER TABLE IF EXISTS public.answers
     OWNER to postgres;
 	
--- Table: public.questions
 
--- DROP TABLE IF EXISTS public.questions;
-
-CREATE TABLE IF NOT EXISTS public.questions
-(
-    question_id numeric(999,0) NOT NULL,
-    test_id numeric(999,0) NOT NULL,
-    question character varying(40) COLLATE pg_catalog."default",
-    CONSTRAINT questions_pkey PRIMARY KEY (question_id),
-    CONSTRAINT questions_test_id_fkey FOREIGN KEY (test_id)
-        REFERENCES public.test (test_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.questions
-    OWNER to postgres;
-
--- Table: public.test
-
--- DROP TABLE IF EXISTS public.test;
-
-CREATE TABLE IF NOT EXISTS public.test
-(
-    test_id numeric(999,0) NOT NULL,
-    test_name character varying(256) COLLATE pg_catalog."default" NOT NULL,
-    descx character varying(999) COLLATE pg_catalog."default",
-    CONSTRAINT test_pkey PRIMARY KEY (test_id)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.test
-    OWNER to postgres;	
